165d9f9f45fc319c6695e664b16ef3dd4cfc2da9
ANZIE05 <161513339+ANZIE05@users.noreply.github.com>
2025-01-17 23:32:52 +0500

    submt

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -4,5 +4,6 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2025-01-09",
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
